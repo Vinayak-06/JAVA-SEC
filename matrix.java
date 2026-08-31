49879a1e3930b9435996236be4320e266fb05b81
Vinayak-06 <vinayakkumar57989@gmail.com>
2026-08-31 20:07:59 +0530

    Implement matrix input and output functionality
    This program prompts the user to enter the number of rows and columns for a matrix, reads the matrix elements, and then prints the matrix.

---
+++ matrix.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int columns = sc.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter matrix elements:");
+
+        // Reading matrix elements
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Printing matrix
+        System.out.println("The matrix is:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
